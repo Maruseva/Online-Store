3bd67a44b2f30b92a4910a58dfc31732d1a5a70e
maruseva <maruseva@yandex.ru>
2023-01-05 22:17:18 +0300

    ON-5: footer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { Footer } from './components/footer/footer.view';
 import { HeaderView } from './components/header/header.view';
 
 const header = new HeaderView('root');
 header.render();
+
+const footer = new Footer('root');
+footer.render();

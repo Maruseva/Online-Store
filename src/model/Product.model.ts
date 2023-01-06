1bd6a2c0f9eeea8df69e10b051206257dac7b31b
Maruseva <101499433+Maruseva@users.noreply.github.com>
2023-01-06 20:31:32 +0300

    On-9: add product.model (#7)
    * On-9: add product.model
    
    * ON-9: rename data

---
+++ src/model/Product.model.ts
@@ -0,0 +1,21 @@
+import data from '../assets/data/products.json';
+
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
+
+export class ProductModel {
+    public getAll(): Product[] {
+        return data.products;
+    }
+}

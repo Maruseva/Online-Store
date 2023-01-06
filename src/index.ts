44998a5f41080c2f681b41773119f88c9d9d4207
maruseva <maruseva@yandex.ru>
2023-01-06 20:19:23 +0300

    исправила ошибку языка

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { Footer } from './components/footer/footer.view';
-import { HeaderView } from './components/header/header.view';
+import { App } from './components/app/App';
 
-const header = new HeaderView('root');
-header.render();
-
-const footer = new Footer('root');
-footer.render();
+const app = new App('root');
+app.run();

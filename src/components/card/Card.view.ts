8450d6abd00abfc84aee1962362815efd0de2133
maruseva <maruseva@yandex.ru>
2023-01-07 11:48:32 +0300

    ON-11: fix card

--- src/components/card/Card.view.ts
+++ src/components/card/Card.view.ts
@@ -1,14 +1,21 @@
 import './card.style.css';
 
 export class Card {
-    public render(title: string): void {
+    private readonly id: string;
+    constructor(id: string) {
+        this.id = id;
+    }
+    public render(title: string, content: string = ''): void {
+        const container = <HTMLElement>document.getElementById(this.id);
         const item = <HTMLDivElement>document.createElement('div');
         const itemTitle = <HTMLHeadingElement>document.createElement('h3');
         const itemContent = <HTMLDivElement>document.createElement('div');
         item.className = 'card_item';
         itemContent.className = 'card_itemContent';
         itemTitle.innerText = title;
+        itemContent.innerHTML = content;
         item.appendChild(itemTitle);
         item.appendChild(itemContent);
+        container.appendChild(item);
     }
 }

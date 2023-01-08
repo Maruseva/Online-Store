30aae911414d87ff145a200c92266ce904b22e18
maruseva <maruseva@yandex.ru>
2023-01-08 13:01:14 +0300

    ON-10: delete mock data

--- src/components/app/App.ts
+++ src/components/app/App.ts
@@ -1,26 +1,5 @@
-import { ProductCard } from '../card/productCard.view';
 import { Footer } from '../footer/footer.view';
 import { HeaderView } from '../header/header.view';
-
-const a = {
-    id: 1,
-    title: 'iPhone 9',
-    description: 'An apple mobile which is nothing like apple',
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: 'Apple',
-    category: 'smartphones',
-    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    images: [
-        'https://i.dummyjson.com/data/products/1/1.jpg',
-        'https://i.dummyjson.com/data/products/1/2.jpg',
-        'https://i.dummyjson.com/data/products/1/3.jpg',
-        'https://i.dummyjson.com/data/products/1/4.jpg',
-        'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    ],
-};
 
 export class App {
     private readonly id: string;
@@ -30,9 +9,6 @@ export class App {
     public run(): void {
         const header = new HeaderView(this.id);
         header.render();
-
-        const prcard = new ProductCard(this.id);
-        prcard.renderProduct(a);
 
         const footer = new Footer(this.id);
         footer.render();

56d9a28186e1bd41865b0a977ebfbee2a9966d95
maruseva <maruseva@yandex.ru>
2023-01-11 19:37:08 +0300

    ON-14: add listCard

--- src/components/listCard/listCard.view.ts
+++ src/components/listCard/listCard.view.ts
@@ -1,33 +1,38 @@
-import { Product } from '../../model/product.model';
 import { Card } from '../card/card.view';
+import './listCard.style.css';
+
+export interface Options {
+    category: string;
+    totalQuantity: number;
+    displayQuantity: number;
+}
 
 export class ListCard extends Card {
-    public render(title: string, products: Product[]): void {
-        const options: string[] = [];
+    public render(title: string, options: Options[]): void {
         let content: string = '<form>';
 
-        products.forEach((el) => {
-            for (let i = 0; options.length < i; i++) {
-                if (el[title] === options[i]) {
-                    break;
-                } else {
-                    options.push(el[title]);
-                }
-            }
-        });
-
-        console.log(options);
+        // products.forEach((el) => {
+        //     for (let i = 0; options.length < i; i++) {
+        //         if (el[title] === options[i]) {
+        //             break;
+        //         } else {
+        //             options.push(el[title]);
+        //         }
+        //     }
+        // });
 
         options.forEach((element) => {
-            content += `<label>
-            <input type="checkbox" name=${title} value=${element}>
-            ${element}
+            content += `<div class="options">
+            <label>
+            <input type="checkbox" name=${title} value=${element.category}>
+            ${element.category}
             </label>
-            <br>`;
+            <span>(${element.displayQuantity}/${element.totalQuantity})</span>
+            </div>`;
         });
 
         content += '</form>';
 
-        super.renderCard(title, content);
+        super.renderCard(title, content, '');
     }
 }

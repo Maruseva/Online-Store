e30d333fae933057e2fe6c37bddc865bcc2819bf
maruseva <maruseva@yandex.ru>
2023-01-11 21:24:34 +0300

    fix parametr imgUrl

--- src/components/card/Card.view.ts
+++ src/components/card/Card.view.ts
@@ -8,15 +8,19 @@ export class Card {
     public renderCard(title: string, content: string = '', imgUrl?: string): void {
         const container = <HTMLElement>document.getElementById(this.id);
         const item = <HTMLDivElement>document.createElement('div');
-        const img = <HTMLImageElement>document.createElement('img');
         const itemTitle = <HTMLHeadingElement>document.createElement('h3');
         const itemContent = <HTMLDivElement>document.createElement('div');
         item.className = 'card_item';
         itemContent.className = 'card_itemContent';
-        img.setAttribute('src', imgUrl);
         itemTitle.innerText = title;
         itemContent.innerHTML = content;
-        item.appendChild(img);
+
+        if (imgUrl) {
+            const img = <HTMLImageElement>document.createElement('img');
+            img.setAttribute('src', imgUrl);
+            item.appendChild(img);
+        }
+
         item.appendChild(itemTitle);
         item.appendChild(itemContent);
         container.appendChild(item);

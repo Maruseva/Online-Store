44887e74cd5d94d02ee572f4136feef826e88390
maruseva <maruseva@yandex.ru>
2023-01-12 21:47:55 +0300

    fiw sliderCard

--- src/components/sliderCard/sliderCard.view.ts
+++ src/components/sliderCard/sliderCard.view.ts
@@ -8,15 +8,17 @@ export interface Parameter {
 
 export class SliderCard extends Card {
     public render(title: string, parameter: Parameter) {
+        const { min, max } = parameter;
+
         const content = `<div class="slider_wrap">
-        <div class="slider_text"><span>${parameter.min}</span>
+        <div class="slider_text"><span>${min}</span>
         <span>&#8660;</span>
-        <span>${parameter.max}</span>
+        <span>${max}</span>
         </div>
         <div class="range_container">
     <div class="sliders_control">
-       <input id="fromSlider" type="range" value="10" min="0" max="100"/>
-       <input id="toSlider" type="range" value="30" min="0" max="100"/>
+       <input id="fromSlider" type="range" min="${min}" max="${max}"/>
+       <input id="toSlider" type="range" min="${min}" max="${max}"/>
     </div>
 </div>
         </div>`;

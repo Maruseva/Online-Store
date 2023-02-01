9ba03febe209c4c02c765567376d8b48eace3120
maruseva <maruseva@yandex.ru>
2023-02-01 15:22:49 +0300

    add ON-37

--- src/components/header/header.view.ts
+++ src/components/header/header.view.ts
@@ -1,11 +1,14 @@
 import template from './header.template.html';
 import './header.style.css';
 import { changePagesUrl } from '../../utils/url';
+import { CartService } from '../../service/cart.service';
 
 export class HeaderView {
     private readonly id: string;
+    private service: CartService;
     constructor(id: string) {
         this.id = id;
+        this.service = new CartService();
     }
     public render(): void {
         const body = <HTMLBodyElement>document.getElementById(this.id);
@@ -24,5 +27,17 @@ export class HeaderView {
             const url = window.location.origin;
             changePagesUrl(url, 'cart');
         });
+
+        const products = this.service.getProducts();
+
+        if (products.length) {
+            let priceAll = 0;
+            products.forEach((element) => (priceAll += element.price));
+            const price = <HTMLSpanElement>document.querySelector('span[class="header_price"]');
+            price.innerHTML = `&#8364;${priceAll}`;
+
+            const number = <HTMLDivElement>document.querySelector('div[class="header_numder"]');
+            number.innerHTML = products.length.toString();
+        }
     }
 }
